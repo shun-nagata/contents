c393e5d76ef3bc2757acbe7aa6f5be349c5e4cf0
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2017-10-03 17:00:36 +0900

    メソッド呼び出しのサンプルコード

--- JavaProg/src/main/java/javaprog/text/class_structure/クラス構造.java
+++ JavaProg/src/main/java/javaprog/text/class_structure/クラス構造.java
@@ -2,23 +2,26 @@ package javaprog.text.class_structure;
 
 class クラス構造 {
 
-  static class Keisan {
+  static class Main {
+    public static void main(String[] args) {
+      Keisan2 keisan = new Keisan2();
 
-    戻り値の型 メソッド名(引数の型 引数1, 引数の型... 引数2) {
-      // 処理
-      return null;
+      float ans = keisan.tenths(20);
+      System.out.println(ans);
+
+      keisan.print();
     }
+  }
+
+  static class Keisan2 {
 
     void print() {
-      System.out.println("電卓です");
+      int ans = square(2, 4);
+      System.out.println(ans);
     }
 
-    int add() {
-      return 1 + 2;
-    }
-
-    int treble(int a) {
-      return a * 3;
+    float tenths(int a) { 
+      return a / 10.0F; 
     }
 
     int square(int a, int b) {
@@ -27,8 +30,30 @@ class クラス構造 {
 
   }
 
-  static class 戻り値の型{};
+  static class Keisan1 {
 
-  static class 引数の型{};
+    戻り値の型 メソッド名(引数の型 引数1, 引数の型... 引数2) {
+      // 処理
+      return null;
+    }
 
+    void print() {
+      System.out.println("計算クラスです");
+    }
+
+    int add() {
+      return 1 + 2;
+    }
+
+    float tenths(int a) { 
+      return a / 10.0F; 
+    }
+
+    int square(int a, int b) {
+      return a * b;
+    }
+
+  }
+  static class 戻り値の型 {};
+  static class 引数の型 {};
 }
