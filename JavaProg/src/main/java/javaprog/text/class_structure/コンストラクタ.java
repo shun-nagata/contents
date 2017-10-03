5a411f1a5b0780a07e899ec0028e79fae611cda2
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2017-10-03 23:33:23 +0900

    コンストラクタの利用例を追加

--- JavaProg/src/main/java/javaprog/text/class_structure/コンストラクタ.java
+++ JavaProg/src/main/java/javaprog/text/class_structure/コンストラクタ.java
@@ -25,4 +25,13 @@ public class コンストラクタ {
     }
 
   }
+
+  static class Main {
+    public static void main(String[] args) {
+      Cat cat1 = new Cat();
+      System.out.println(cat1.name + ", " + cat1.age);
+      Cat cat2 = new Cat("Hikari", 3);
+      System.out.println(cat2.name + ", " + cat2.age);
+    }
+  }
 }
