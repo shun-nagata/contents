bdc9a78ae651505c1563b7ea37ba248732fbc6ff
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2017-10-03 14:26:09 +0900

    change indents

--- JavaProg/src/main/java/javaprog/text/class_structure/基本型と参照型.java
+++ JavaProg/src/main/java/javaprog/text/class_structure/基本型と参照型.java
@@ -2,19 +2,20 @@ package javaprog.text.class_structure;
 
 public class 基本型と参照型 {
 
-	static class Main {
-		public static void main(String[] args) {
-			int a = 10;
-			int b = 20;
-			int c = a;
+  static class Main {
+    public static void main(String[] args) {
+      int a = 10;
+      int b = 20;
+      int c = a;
 
-			Foo f1 = new Foo();
-			Foo f2 = new Foo();
-			Foo f3 = f1;
-		}
-	}
+      Foo f1 = new Foo();
+      Foo f2 = new Foo();
+      Foo f3 = f1;
+    }
+  }
 
-	static class Foo {}
+  static class Foo {
+  }
 
 
 }
