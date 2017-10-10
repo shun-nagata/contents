4b9039dcb9a90e6bd19e228470c9c3ceee85a0b8
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2017-10-10 12:25:28 +0900

    ワークシート1問目を追加

---
+++ JavaProg/src/main/java/javaprog/worksheet/lec03/Lec03A.java
@@ -0,0 +1,31 @@
+package javaprog.worksheet.lec03;
+
+import java.util.Random;
+
+class Lec03A {
+
+  public static void main(String[] args) {
+    int max = 8;
+    int[] r = new int[max];
+
+    // 0〜99の乱数をrに代入する
+    Random rand = new Random();
+    for (int i = 0; i < max; i++) {
+      r[i] = rand.nextInt(100);
+    }
+
+    int min = r[0];
+    for (int j = 1; j < max; j++) {
+      if (r[j] < min) {
+        min = r[j];
+      }
+    }
+
+    System.out.print("配列の要素は ");
+    for (int ri : r) {
+      System.out.print(ri + " ");
+    }
+    System.out.println("\n最小値は " + min);
+  }
+
+}
