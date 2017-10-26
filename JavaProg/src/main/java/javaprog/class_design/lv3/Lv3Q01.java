6d7eac1b4b596e3157a21276ab62d9ea95b5ec85
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2017-10-27 00:11:47 +0900

    Lv3まで完成

---
+++ JavaProg/src/main/java/javaprog/class_design/lv3/Lv3Q01.java
@@ -0,0 +1,20 @@
+package javaprog.class_design.lv3;
+
+class Lv3Q01 {
+
+  public static class Capsule {
+
+    private String item1;
+    private int item2;
+
+    public void do1() {
+      //　処理は省略
+    }
+
+    public void do2() {
+      //　処理は省略
+    }
+
+  }
+
+}
---
+++ JavaProg/src/main/java/javaprog/class_design/lv3/Lv3Q05.java
@@ -0,0 +1,24 @@
+package javaprog.class_design.lv3;
+
+class Lv3Q05 {
+
+  public static class Capsule {
+
+    private String item1;
+    private int item2;
+
+    public Capsule(String item1, int item2) {
+      //　処理は省略
+    }
+
+    public void do1() {
+      //　処理は省略
+    }
+
+    public void do2() {
+      //　処理は省略
+    }
+
+  }
+
+}
